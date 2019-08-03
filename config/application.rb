6307573274bc40926361bf5be210a50ce50ecfa1
Rien Croonenborghs <rien@croonenborghs.net>
2019-08-03 15:24:10 +1200

    asd

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,10 @@ module OpendirectoriesDownloaderServer
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        origins "*"
+        resource "*",
+          headers: :any,
+          methods: %i(get post put patch delete options head)
       end
     end
   end
