ead57135bf0729c31a6d25334c413f34b721b326
Rien Croonenborghs <rien@croonenborghs.net>
2019-08-04 14:23:23 +1200

    resque

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module OpendirectoriesServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.active_job.queue_adapter = :resque
   end
 end
